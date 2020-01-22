08e634075301e2d7b48dcc0c8f862c00d130d426
Herve Frezza-Buet <herve.frezza-buet@centralesupelec.fr>
2020-01-22 17:15:36 +0100

    transfert

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 This is a set of C++ tools for designing 2D demos. It is based on opencv.
 
+
+vq3 is a result of the <a href="http://interreg-grone.eu">GRONE project</a>, supported by the Interreg "Grande Région" program of the European Union's European Regional Development Fund.
+
+# Unix Installation
+
+First, get the files.
+
+``` 
+git clone https://github.com/HerveFrezza-Buet/demo2d
+``` 
+
+Then, you can install the two packages as follows.
+
+```
+mkdir -p demo2d/build
+cd demo2d/build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+sudo make install
+cd ../..
+```
+
+The documentation is in /usr/share/doc/demo2d
---
+++ src/demo2d.hpp
@@ -0,0 +1,43 @@
+/*
+ *   Copyright (C) 2020,  CentraleSupelec
+ *
+ *   Author : Hervé Frezza-Buet
+ *
+ *   Contributor :
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public
+ *   License (GPL) as published by the Free Software Foundation; either
+ *   version 3 of the License, or any later version.
+ *   
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *   General Public License for more details.
+ *   
+ *   You should have received a copy of the GNU General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *   Contact : herve.frezza-buet@centralesupelec.fr
+ *
+ */
+
+#pragma once
+
+#include <demo2dPoint.hpp>
+#include <demo2dCV.hpp>
+#include <demo2dSample.hpp>
+
+/**
+
+   @mainpage
+
+   The demo2d package is a result of the <a href="http://interreg-grone.eu">GRONE project</a>, supported by the Interreg "Grande Région" program of the European Union's European Regional Development Fund.
+
+*/
+
+
+/**
+   @example toto.cpp
+*/
